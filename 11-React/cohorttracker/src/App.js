7cefa0b7e0d2c6e92be6d590ffdd092135dac53e
Divija Pilla <divijacoding@gmail.com>
2026-07-28 01:12:28 +0530

    Added React Exercise 5 - Cohort Tracker

---
+++ 11-React/cohorttracker/src/App.js
@@ -0,0 +1,19 @@
+import cohorts from "./data/cohorts";
+import CohortDetails from "./components/CohortDetails";
+
+function App() {
+  return (
+    <div>
+      <h1>Cohorts Details</h1>
+
+      {cohorts.map((cohort) => (
+        <CohortDetails
+          key={cohort.id}
+          cohort={cohort}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
